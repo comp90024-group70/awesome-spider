6de92c01286dd5fcdcc559a41859b5ecffee0b19
zian.wang <913248383@qq.com>
2023-05-21 13:40:41 +1000

    Update: extract request database url by using environment; Update dockerfile

--- au/db.py
+++ au/db.py
@@ -1,9 +1,13 @@
 import couchdb
 from uuid import uuid4
+import os
 
-COUCH_SERVER = "http://172.26.136.13:5984"
-server = couchdb.Server(COUCH_SERVER)
-server.resource.credentials = ("admin", "wza7626222")
+COUCHDB_DOMAIN = os.environ.get("COUCHDB_DOMAIN", "172.26.131.154")
+COUCHDB_USER = os.environ.get("COUCHDB_USER", "admin")
+COUCHDB_PASSWORD = os.environ.get("COUCHDB_PASSWORD", "wza7626222")
+COUCHDB_SERVER = f'http://{COUCHDB_DOMAIN}:5984'
+server = couchdb.Server(COUCHDB_SERVER)
+server.resource.credentials = (COUCHDB_USER, COUCHDB_PASSWORD)
 
 
 def save_data(data: dict):
